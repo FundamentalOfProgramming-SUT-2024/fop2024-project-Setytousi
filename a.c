8f6e4985d78d35ee806de9a25c58e1f6e9dc8d28
Setytousi <setytousi@gmail.com>
2025-01-02 21:21:07 +0330

    menu

--- a.c
+++ a.c
@@ -3,41 +3,138 @@
 #include <ncurses.h>
 #include <unistd.h>
 
-void draw_border(){
+void draw_border_menu(){
     attron(COLOR_PAIR(1));
-    for (int i = 0; i < LINES; i++){
-        mvprintw(i, 0, " ");
-        mvprintw(i, COLS - 1," ");
+    for (int i = 0; i <= 20; i++){
+        mvprintw(i, COLS / 2 - 30, " ");
+        mvprintw(i, COLS / 2 + 30," ");
     }
-    for (int i = 0; i < COLS; i++){
+    for (int i = COLS / 2 - 30; i <= COLS / 2 + 30; i++){
         mvprintw(0, i," ");
-        mvprintw(LINES - 1, i," ");
+        mvprintw(20, i," ");
     }
     refresh();
     attroff(COLOR_PAIR(1));
 }
 
 void menus(){
-    draw_border();
+    draw_border_menu();
     attron(A_BOLD);
     attron(A_BLINK);
     attron(COLOR_PAIR(2));
-    mvprintw(3, (COLS - 5) / 2, "ROGUE");
+    mvprintw(4, (COLS - 5) / 2, "ROGUE");
     attroff(A_BOLD);
     attroff(A_BLINK);
+    attroff(COLOR_PAIR(2));
     int curmenu = 0;
     int total_menus = 5;
     while(1){
-        mvprintw(3, (COLS - 10) / 2, "Create new user");
-        mvprintw(4, (COLS - 10) / 2, "Create new user");
-        mvprintw(5, (COLS - 10) / 2, "Create new user");
-        mvprintw(6, (COLS - 10) / 2, "Create new user");
-        int ch = getch();
-        if (ch == KEY_DOWN && curmenu < total_menus - 1){
-
+        clear();
+        attron(A_BOLD);
+        attron(A_BLINK);
+        attron(COLOR_PAIR(2));
+        mvprintw(4, (COLS - 5) / 2, "ROGUE");
+        attroff(A_BOLD);
+        attroff(A_BLINK);
+        attroff(COLOR_PAIR(2));
+        draw_border_menu();
+        if (curmenu == 0){
+            attron(COLOR_PAIR(3));
+            attron(A_BLINK);
+            mvprintw(6, (COLS - 10) / 2, "Create new user");
+            attroff(COLOR_PAIR(3));
+            attroff(A_BLINK);
         }
+        else{
+            attron(COLOR_PAIR(2));
+            attron(A_BLINK);
+            mvprintw(6, (COLS - 10) / 2, "Create new user");
+            attroff(COLOR_PAIR(2));
+            attroff(A_BLINK);
+        }
+        if (curmenu == 1){
+            attron(COLOR_PAIR(3));
+            attron(A_BLINK);
+            mvprintw(7, (COLS - 10) / 2, "Login");
+            attroff(COLOR_PAIR(3));
+            attroff(A_BLINK);
+        }
+        else{
+            attron(COLOR_PAIR(2));
+            attron(A_BLINK);
+            mvprintw(7, (COLS - 10) / 2, "Login");
+            attroff(COLOR_PAIR(2));
+            attroff(A_BLINK);
+        }
+        if (curmenu == 2){
+            attron(COLOR_PAIR(3));
+            attron(A_BLINK);
+            mvprintw(8, (COLS - 10) / 2, "Guest User");
+            attroff(COLOR_PAIR(3));
+            attroff(A_BLINK);
+        }
+        else{
+            attron(COLOR_PAIR(2));
+            attron(A_BLINK);
+            mvprintw(8, (COLS - 10) / 2, "Guest User");
+            attroff(COLOR_PAIR(2));
+            attroff(A_BLINK);
+        }
+        if (curmenu == 3){
+            attron(COLOR_PAIR(3));
+            attron(A_BLINK);
+            mvprintw(9, (COLS - 10) / 2, "Scoreboard");
+            attroff(COLOR_PAIR(3));
+            attroff(A_BLINK);
+        }
+        else{
+            attron(COLOR_PAIR(2));
+            attron(A_BLINK);
+            mvprintw(9, (COLS - 10) / 2, "Scoreboard");
+            attroff(COLOR_PAIR(2));
+            attroff(A_BLINK);
+        }
+        if (curmenu == 4){
+            attron(COLOR_PAIR(3));
+            attron(A_BLINK);
+            mvprintw(10, (COLS - 10) / 2, "Exit");
+            attroff(COLOR_PAIR(3));
+            attroff(A_BLINK);
+        }
+        else{
+            attron(COLOR_PAIR(2));
+            attron(A_BLINK);
+            mvprintw(10, (COLS - 10) / 2, "Exit");
+            attroff(COLOR_PAIR(2));
+            attroff(A_BLINK);
+        }
+        int ch = getch();
+        if (ch == KEY_UP){
+            curmenu--;
+            curmenu = (curmenu + total_menus) % total_menus;
+        }
+        else if (ch == KEY_DOWN){
+            curmenu++;
+            curmenu %= total_menus;
+        }
+        else if (ch == KEY_ENTER){
+            // if (curmenu == 0){
+            //     new_user_menu();
+            //     return;
+            // }
+            // else if (curmenu == 1){
+            //     login_menu();
+            // }
+        }
+        else{
+            attron(COLOR_PAIR(4));
+            mvprintw(1, COLS / 2 - 29, "Invalid Key !");
+            refresh();
+            sleep(1);
+            attroff(COLOR_PAIR(4));
+        }
+        refresh();
     }
-    refresh();
 }
 
 
@@ -45,10 +142,13 @@ int main(){
     initscr();
     keypad(stdscr, TRUE);
     noecho();
+    curs_set(0);
     if (has_colors()){
         start_color();
         init_pair(1, COLOR_WHITE, COLOR_WHITE);
         init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+        init_pair(3, COLOR_YELLOW, COLOR_WHITE);
+        init_pair(4, COLOR_RED, COLOR_BLACK);
     }
     else return 0;
     menus();
